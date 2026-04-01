300f47300de7ba610702722352ce6904626029e9
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2026-04-01 08:47:36 -0300

    change lib variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,13 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { APP_FILTER } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { PrismaModule } from './global/prisma/prisma.module';
 import { PrismaExceptionFilter } from './global/error/prismaclientknownerror.exception';
-import { JwtAuthMiddleware } from './global/middleware/jwtAuth.middleware';
-import { SsoAuthGuard } from './global/guard/ssoAuthGuard.guard';
 
 import { EquipmentModule } from './routes/equipment/equipment.module';
 import { ClientModule } from './routes/client/client.module';
@@ -23,13 +16,11 @@ import { ELeaseModule } from './routes/elease/elease.module';
 import { DocumentModule } from './routes/document/document.module';
 import { FinantialModule } from './routes/finantial/finantial.module';
 import { AccessoryModule } from './routes/accessory/accessory.module';
-import { AuthModule } from './routes/auth/auth.module';
-import { RedisModule } from './global/redis/redis.module';
+
 @Module({
   imports: [
     HttpModule,
     PrismaModule,
-    RedisModule,
     EquipmentModule,
     ConfigModule,
     ClientModule,
@@ -38,20 +29,13 @@ import { RedisModule } from './global/redis/redis.module';
     DocumentModule,
     FinantialModule,
     AccessoryModule,
-    AuthModule,
+    SsoClientModule,
   ],
   controllers: [AppController],
   providers: [
     AppService,
     ConfigService,
     { provide: APP_FILTER, useClass: PrismaExceptionFilter },
-    { provide: APP_GUARD, useClass: SsoAuthGuard },
   ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(JwtAuthMiddleware)
-      .forRoutes({ path: '(.*)', method: RequestMethod.ALL });
-  }
-}
+export class AppModule {}
